eb4f5bf195bc70ca626a6ab5466f1551d3677b50
paulagardner <paula.dorothyg@gmail.com>
2021-11-01 10:16:15 -0700

    add problem 3 df

--- homework5.R
+++ homework5.R
@@ -1,31 +1,26 @@
-ID <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+###########PART 1 
+
 factorA <- c(0, 0, 0, 0, 0, 1, 1, 1, 1, 1)
 factorB <- c(0, 0, 0, 0, 1, 0, 0, 1, 1, 1)
 responsevariable <- c(1, 2, 3, 6, 5, 3, 5, 8, 9, 10)
 
 df <- data.frame( factorA, factorB, responsevariable)
 df
-  
-((sum(df$responsevariable))^2 / 10 ) - (  (   sum(factorA*      (sum(df$responsevariable))   )^2   ))
-
-(sum(0 + 1 + 2 + 3 + 6 + 5 + 3 + 5 + 8 + 9 + 10 )^2)/10 - ((0 + 1 + 0+ 2 + 0 + 3 + 0 + 6 + 0 + 5 + 1 + 3 + 1 + 5 + 1 + 8 + 1 + 9 + 1 + 10)^2)/(sum(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)) 
-#double sum is just indexing across a matrix .... 
-#the above is still missing a sum-- not quite getting it conceptually, ask jen-yu?
-
-
 
 sum(responsevariable[1:5]) #trying to do this as sum(df$responsevariable)) returns a error: object of type 'closure' is not subsettable
 sum(responsevariable[6:10])
 
 
-(    (( sum(responsevariable[1:5])^2 )/5)   +  ( sum( responsevariable[6:10] )^2)/5     )           -         (  ((sum(responsevariable[1:5]) + sum(responsevariable[6:10])) ^2 ) / (5 + 5) )      
+(    (( sum(responsevariable[1:5])^2 )/5)   +  ( sum( responsevariable[6:10] )^2)/5     )           -         (  ((sum(responsevariable[1:5]) + sum(responsevariable[6:10])) ^2 ) / (5 + 5) ) 
+
+(    (( sum(responsevariable[1:5])^2 )/5)   +  ( sum( responsevariable[6:10] )^2)/5     )           -         (((  sum(1 + 2 + 3 + 6 + 5) + sum(3 + 5 + 8 + 9 + 10) ) ^2 ) / (5 + 5) )  #conceptually/making sure the above is doing what I'm expecting
 
 
 
+observations <- c(1.25, 0.88, 1.22, 1.15, 1.41, 0.98, 1.30, 1.19, 1.43, 1.07, 1.37, 1.44, 1.43, 1.43, 1.42, 1.41)
+seedsize <- c(1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4)
+colony <- c('s', 's', 's', 's', 'm', 'm', 'm', 'm', 'l', 'l', 'l', 'l', 'xl', 'xl', 'xl', 'xl')
 
-
-
-#######################################
-#part b 
-
-#####
+df3 <- data.frame(observations, seedsize, colony)
+df3
